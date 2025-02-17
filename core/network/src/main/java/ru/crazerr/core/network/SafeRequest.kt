f17c9d56f541aadd9edbea8436d6311e40408019
anton_fadeev <antonfade3v@yandex.ru>
2025-02-17 15:12:39 +0300

    feature(network): add safe methods for get, post, put and delete requests

--- core/network/src/main/java/ru/crazerr/core/network/SafeRequest.kt
+++ core/network/src/main/java/ru/crazerr/core/network/SafeRequest.kt
@@ -4,11 +4,12 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.request
-import io.ktor.client.statement.HttpResponse
+import io.ktor.client.request.url
+import io.ktor.http.HttpMethod
 import io.ktor.utils.io.CancellationException
 
 suspend inline fun <reified T> HttpClient.safeRequest(
-    block: HttpRequestBuilder.() -> HttpResponse
+    block: HttpRequestBuilder.() -> Unit
 ): Result<T> {
     return try {
         val response = request { block() }
@@ -21,5 +22,49 @@ suspend inline fun <reified T> HttpClient.safeRequest(
         throw e
     } catch (e: Exception) {
         Result.failure(e)
+    }
+}
+
+suspend inline fun <reified T> HttpClient.delete(
+    url: String,
+    block: HttpRequestBuilder.() -> Unit = {}
+): Result<T> {
+    return safeRequest {
+        this.url(url)
+        this.method = HttpMethod.Delete
+        block()
+    }
+}
+
+suspend inline fun <reified T> HttpClient.post(
+    url: String,
+    block: HttpRequestBuilder.() -> Unit = {}
+): Result<T> {
+    return safeRequest {
+        this.url(url)
+        this.method = HttpMethod.Post
+        block()
+    }
+}
+
+suspend inline fun <reified T> HttpClient.get(
+    url: String,
+    block: HttpRequestBuilder.() -> Unit = {}
+): Result<T> {
+    return safeRequest {
+        this.url(url)
+        this.method = HttpMethod.Get
+        block()
+    }
+}
+
+suspend inline fun <reified T> HttpClient.put(
+    url: String,
+    block: HttpRequestBuilder.() -> Unit = {}
+): Result<T> {
+    return safeRequest {
+        this.url(url)
+        this.method = HttpMethod.Put
+        block()
     }
 }
